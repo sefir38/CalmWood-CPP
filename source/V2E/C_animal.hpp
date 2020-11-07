1cdc9d340f7540af053c95411dbfa12a50593bab
ali <semir301@hotmail.com>
2020-11-07 16:34:31 +0100

    essaie aucun changement

--- source/V2E/C_animal.hpp
+++ source/V2E/C_animal.hpp
@@ -121,6 +121,7 @@ protected :
         int attack ( Environment * environment, std::unordered_multimap<int, Animal *> * VisibleAnimals, int intruderX, int intruderY, int specie );
         int growth ( Environment * environment );
         int dead ( Environment * environment );
+        int predate ( Environment * environment, std::unordered_multimap<int, Animal *> * VisibleAnimals, int intruderX, int intruderY, int specie );
 
 };
 
@@ -145,7 +146,14 @@ protected :
 class Hyla: public Animal
 {
 public :
-        Hyla ( int id = 1, std::string newName = "hyla" ) :Animal ( id, newName ) {}
+        Hyla ( int id = 1, 
+        std::string newName = "hyla",
+        std::vector<unsigned int> lifeCycle = {1,24,1},
+        std::vector<int> probabilities = {80,70,20,0,40,10,0,60},
+        std::vector<int> detectionRadius = {1,1,2},
+        std::vector<int> actionRadius = {1,1,1},
+        bool isBorn = false,
+        int newSpawnNumber = 100 ) :Animal ( id, newName ,lifeCycle, probabilities, detectionRadius, actionRadius, isBorn, newSpawnNumber) {}
         ~Hyla() {};
 protected :
        int decision ( Environment * environment, std::vector<std::unordered_multimap<int, Animal *>> * VisibleAnimals, std::vector<std::unordered_multimap<int, Plant * >> * VisiblePlants, std::vector<std::vector<int>> * CellSpecs );
@@ -154,7 +162,7 @@ protected :
 class Phengaris: public Animal
 {
 public :
-        Phengaris ( int id = 2, std::string newName = "phengaris" ) :Animal ( id, newName ) {}
+        Phengaris ( int id = 2, std::string newName = "phengaris") :Animal ( id, newName ) {}
         ~Phengaris() {};
 protected :
         int decision ( Environment * environment, std::vector<std::unordered_multimap<int, Animal *>> * VisibleAnimals, std::vector<std::unordered_multimap<int, Plant * >> * VisiblePlants, std::vector<std::vector<int>> * CellSpecs );
@@ -172,7 +180,15 @@ protected :
 class Vipera: public Animal
 {
 public :
-        Vipera ( int id = 4, std::string newName = "vipera" ) :Animal ( id, newName ) {}
+        Vipera ( int id = 4, 
+        std::string newName = "vipera",
+        std::vector<unsigned int> lifeCycle = {1,24,1},
+        std::vector<int> probabilities = {80,70,20,0,40,10,0,60},
+        std::vector<int> detectionRadius = {1,1,2},
+        std::vector<int> actionRadius = {1,1,1},
+        bool isBorn = false,
+        int newSpawnNumber = 100 
+        ) :Animal ( id, newName ) {}
         ~Vipera() {};
 protected :
         int decision ( Environment * environment, std::vector<std::unordered_multimap<int, Animal *>> * VisibleAnimals, std::vector<std::unordered_multimap<int, Plant * >> * VisiblePlants, std::vector<std::vector<int>> * CellSpecs );
